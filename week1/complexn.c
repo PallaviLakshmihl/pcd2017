dd72ccfc22650742c576c489def473c90ae5e98c
Your Name <you@example.com>
2017-02-26 16:17:36 +0530

    add n complex number

--- week1/complexn.c
+++ week1/complexn.c
@@ -15,20 +15,16 @@ struct complex add(int n,struct complex a[n]) {
 int main() {
         int n;
         struct complex a[n],sum;
-        printf("Enter the number of complex numbers 
-:");
+        printf("Enter the number of complex numbers :");
         scanf("%d",& n);
         printf("Enter %d imaginary numbers ",n);
         for(int i = 0 ; i<n ;i++)
         {
-                printf("Enter real part of %d 
-complex numbe$
+                printf("Enter real part of %d complex number :");
                 scanf("%f",& a[i].real);
-                printf("Enter imaginary part of %d 
-complex $
+                printf("Enter imaginary part of %d complex number :");
                 scanf("%f",& a[i].img);
         }
         sum = add(n,a);
-        printf("Sum of %d complex numbers is %f + 
-i%f",n,su$
+        printf("Sum of %d complex numbers is %f + i%f",n,sum.real,sum.img);
 }
