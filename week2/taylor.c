a01813379f87155f032c8c593a6536ec9daa30ac
Your Name <you@example.com>
2017-03-11 19:02:46 +0530

    taylor sin

---
+++ week2/taylor.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+# include<math.h>
+float convertRadians(float d)
+{
+	float radian ;
+	radian = (3.1412/180)*d;
+	return radian;
+}
+float computeSin(int x)
+{
+	float term ,sum, diff, prev;
+	int i;
+	sum = term = diff = x;
+	for(i = 3 ;diff>0.000001;i++)
+	{
+		prev = term ;
+		term = (-term * x *x)/(i*(i-1));
+		diff = fabs(prev-term);
+		sum = sum +term;
+	}
+	return sum;
+}
+int main()
+{
+	float degree , x , sum ;
+	printf("Enter the degree : ");
+	scanf("%f",&degree);
+	x = convertRadians(degree);
+	sum = computeSin(x);
+	printf("sin(%f) = %f \n",degree ,sum);
+	return 0;
+}
