4a0fc2d5afed7d20d94dde6f5eef9f508c877d61
Your Name <you@example.com>
2017-03-20 17:03:37 +0530

    right rotate

--- week3/rightRotate2.c
+++ week3/rightRotate2.c
@@ -2,5 +2,18 @@
 unsigned int rightRotate(unsigned int x ,unsigned int n)
 {
 	n = n%(sizeof(int)*8);
-	int l = 
+	int l = (sizeof(int)*8)-n;
+	return (x>>n|x<<l);
+}
+int main() 
+{
+        unsigned x , n , newNum;
+        printf("Enter the number : ");
+        scanf("%u",&x);
+        printf("Enter the number of rotations : ");
+        scanf("%u",&n);
+        newNum = rightRotate(x ,n);
+        printf("The given number is %x \n ",x);
+        printf("The new number afer right rotating by %d is %x\n ",n,newNum);
+	return 0;
 }
